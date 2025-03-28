d21898e0c6d425b47e701386affe3899866670f7
Fazil <faziltirtana@tutanota.com>
2025-03-28 18:31:35 +0700

    feat: initialize spring boot app

---
+++ src/test/java/id/ac/ui/cs/advprog/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
