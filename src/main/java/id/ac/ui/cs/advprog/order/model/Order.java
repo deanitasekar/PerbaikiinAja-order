ec41b6d596f6b45a305d2d4bd7336c38c61e98b7
deanitasekar <deanitasekar@gmail.com>
2025-04-09 09:51:37 +0700

    [REFACTOR] Apply OrderStatus enum for Order model

--- src/main/java/id/ac/ui/cs/advprog/order/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/order/model/Order.java
@@ -1,4 +1,6 @@
 package id.ac.ui.cs.advprog.order.model;
+import id.ac.ui.cs.advprog.order.enums.OrderStatus;
+import id.ac.ui.cs.advprog.order.enums.PaymentMethod;
 
 public class Order {
 
@@ -6,13 +8,25 @@ public class Order {
     private String itemName;
     private String itemCondition;
     private String technician;
-    private String status;
 
-    public Order(String orderId, String itemName, String itemCondition) {
+    private OrderStatus status;
+    private PaymentMethod paymentMethod;
+
+    public Order(String orderId, String itemName, String itemCondition,
+                 String statusStr, String paymentMethodStr) {
         this.orderId = orderId;
         this.itemName = itemName;
         this.itemCondition = itemCondition;
-        this.status = "PENDING";
+
+        if (!OrderStatus.contains(statusStr)) {
+            throw new IllegalArgumentException("Invalid OrderStatus");
+        }
+        this.status = OrderStatus.valueOf(statusStr);
+
+        if (!PaymentMethod.contains(paymentMethodStr)) {
+            throw new IllegalArgumentException("Invalid PaymentMethod");
+        }
+        this.paymentMethod = PaymentMethod.valueOf(paymentMethodStr);
     }
 
     public String getOrderId() {
@@ -31,25 +45,61 @@ public class Order {
         return technician;
     }
 
-    public String getStatus() {
+    public OrderStatus getStatus() {
         return status;
     }
 
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setItemCondition(String itemCondition) {
+        this.itemCondition = itemCondition;
+    }
+
     public void setTechnician(String newTechnician) {
-        if (!"PENDING".equals(this.status)) {
+        if (this.status != OrderStatus.PENDING) {
             throw new IllegalStateException("Teknisi tidak dapat diubah");
         }
         this.technician = newTechnician;
     }
 
-    public void setStatus(String newStatus) {
+    public void setStatus(String newStatusStr) {
+        if (!OrderStatus.contains(newStatusStr)) {
+            throw new IllegalArgumentException("Invalid status");
+        }
+        this.status = OrderStatus.valueOf(newStatusStr);
+    }
+
+
+    public void setStatus(OrderStatus newStatus) {
         this.status = newStatus;
     }
 
+    public void setPaymentMethod(String paymentMethodStr) {
+        if (!PaymentMethod.contains(paymentMethodStr)) {
+            throw new IllegalArgumentException("Invalid payment method");
+        }
+        this.paymentMethod = PaymentMethod.valueOf(paymentMethodStr);
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
     public void cancel() {
-        if (!"PENDING".equals(this.status)) {
+        if (this.status != OrderStatus.PENDING) {
             throw new IllegalStateException("Order tidak dapat dibatalkan");
         }
-        this.status = "CANCELLED";
+        this.status = OrderStatus.CANCELLED;
     }
 }
