e65f74929e28728edec94c3ce72cf22476bb7910
deanitasekar <deanitasekar@gmail.com>
2025-04-09 10:10:00 +0700

    [REFACTOR] Apply OrderStatus enum for OrderBuilder model

--- src/main/java/id/ac/ui/cs/advprog/order/model/OrderBuilder.java
+++ src/main/java/id/ac/ui/cs/advprog/order/model/OrderBuilder.java
@@ -1,10 +1,17 @@
 package id.ac.ui.cs.advprog.order.model;
 
+import id.ac.ui.cs.advprog.order.enums.OrderStatus;
+import id.ac.ui.cs.advprog.order.enums.PaymentMethod;
+
 public class OrderBuilder {
 
     private String orderId;
     private String itemName;
     private String itemCondition;
+    private String technician;
+
+    private OrderStatus status = OrderStatus.PENDING;
+    private PaymentMethod paymentMethod = PaymentMethod.BANK_TRANSFER;
 
     public OrderBuilder withOrderId(String orderId) {
         this.orderId = orderId;
@@ -21,7 +28,35 @@ public class OrderBuilder {
         return this;
     }
 
+    public OrderBuilder withTechnician(String technician) {
+        this.technician = technician;
+        return this;
+    }
+
+    public OrderBuilder withStatus(OrderStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    public OrderBuilder withPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+        return this;
+    }
+
+
     public Order build() {
-        return new Order(orderId, itemName, itemCondition);
+        Order order = new Order(
+                this.orderId,
+                this.itemName,
+                this.itemCondition,
+                this.status.toString(),
+                this.paymentMethod.toString()
+        );
+
+        if (this.technician != null) {
+            order.setTechnician(this.technician);
+        }
+
+        return order;
     }
 }
