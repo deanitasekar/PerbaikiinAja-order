f31be29ff4c8ce6d95e30e00e32a5d66440bcdf1
Fazil <faziltirtana@tutanota.com>
2025-04-10 21:18:15 +0700

    [RED] Add skeleton for Notification Service unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/order/service/NotificationServiceTest.java
@@ -0,0 +1,36 @@
+package id.ac.ui.cs.advprog.order.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import id.ac.ui.cs.advprog.order.repository.NotificationRepository;
+
+public class NotificationServiceTest {
+
+    private NotificationRepository notificationRepository;
+    private NotificationServiceImpl notificationService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testCreateNotification() {
+    }
+
+    @Test
+    void testGetNotificationsMarksAsRead() {
+    }
+
+    @Test
+    void testDeleteNotificationSuccess() throws Exception {
+    }
+
+    @Test
+    void testDeleteNotificationNotFound() {
+    }
+
+    @Test
+    void testDeleteNotificationNotAuthorized() {
+    }
+}
