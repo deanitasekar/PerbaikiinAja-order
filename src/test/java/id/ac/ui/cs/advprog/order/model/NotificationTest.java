090d5b84785707683981990c95edc1c297d45b87
Fazil <faziltirtana@tutanota.com>
2025-04-10 21:09:09 +0700

    [RED] Add skeleton for Notification Model unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/order/model/NotificationTest.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.order.model;
+
+import org.junit.jupiter.api.Test;
+
+public class NotificationTest {
+
+    @Test
+    void testNotificationCreationDefault() {
+    }
+
+    @Test
+    void testNotificationSetters() {
+    }
+}
