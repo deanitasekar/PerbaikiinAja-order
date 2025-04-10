2cdf45a83d91320886431486742be6316c1dd6b8
Fazil <faziltirtana@tutanota.com>
2025-04-10 21:15:55 +0700

    [RED] Add Notification Repository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/order/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.order.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import id.ac.ui.cs.advprog.order.model.Notification;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+}
