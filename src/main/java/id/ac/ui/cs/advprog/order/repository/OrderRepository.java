3e4992b900893b87f4f22ca4bd7f89d6bc331ffe
deanitasekar <deanitasekar@gmail.com>
2025-04-10 23:00:32 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/order/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/order/repository/OrderRepository.java
@@ -3,18 +3,27 @@ package id.ac.ui.cs.advprog.order.repository;
 import id.ac.ui.cs.advprog.order.model.Order;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 public class OrderRepository {
 
+    private final Map<String, Order> orderData = new HashMap<>();
+
     public void save(Order order) {
+        orderData.put(order.getOrderId(), order);
     }
 
     public Order findById(String orderId) {
-        throw new NoSuchElementException("No order found with id: " + orderId);
+        if (!orderData.containsKey(orderId)) {
+            throw new NoSuchElementException("No order found");
+        }
+        return orderData.get(orderId);
     }
 
     public List<Order> findAll() {
-        return null;
+        return new ArrayList<>(orderData.values());
     }
 }
