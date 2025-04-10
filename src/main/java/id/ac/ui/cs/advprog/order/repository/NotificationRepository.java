d49f712cf5131d26243c34418bb97e8163a95c2a
Fazil <faziltirtana@tutanota.com>
2025-04-10 21:16:28 +0700

    [GREEN] Implement Notification Repository

--- src/main/java/id/ac/ui/cs/advprog/order/repository/NotificationRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/order/repository/NotificationRepository.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.order.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import id.ac.ui.cs.advprog.order.model.Notification;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    List<Notification> findByUserId(UUID userId);
 }
