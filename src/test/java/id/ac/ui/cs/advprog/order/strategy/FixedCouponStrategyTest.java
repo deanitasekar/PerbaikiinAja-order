ce414d5fafc4e4ee4246b0c919899fd7764bb66c
kaindraa <pcgsachio@gmail.com>
2025-04-11 20:48:22 +0700

    test: add FixedCouponStrategyTest

---
+++ src/test/java/id/ac/ui/cs/advprog/order/strategy/FixedCouponStrategyTest.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.order.strategy;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FixedCouponStrategyTest {
+
+    @Test
+    void testFixedDiscountAppliedCorrectly() {
+        FixedCouponStrategy strategy = new FixedCouponStrategy();
+        double price = 100.0;
+        double discount = 20.0;
+
+        double result = strategy.apply(price, discount, 0);
+        assertEquals(80.0, result);
+    }
+
+    @Test
+    void testFixedDiscountNotNegative() {
+        FixedCouponStrategy strategy = new FixedCouponStrategy();
+        double result = strategy.apply(10.0, 20.0, 0);
+        assertEquals(0.0, result);
+    }
+}
