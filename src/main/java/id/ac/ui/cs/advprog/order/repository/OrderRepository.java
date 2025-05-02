0b2bdccdef496289db4fa3ee1deaf3d2b061ef43
deanitasekar <deanitasekar@gmail.com>
2025-05-02 17:44:52 +0700

    [RED] Add OrderRepository skeleton

--- src/main/java/id/ac/ui/cs/advprog/order/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/order/repository/OrderRepository.java
@@ -1,29 +1,13 @@
 package id.ac.ui.cs.advprog.order.repository;
 
 import id.ac.ui.cs.advprog.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
 
-public class OrderRepository {
-
-    private final Map<String, Order> orderData = new HashMap<>();
-
-    public void save(Order order) {
-        orderData.put(order.getOrderId(), order);
-    }
-
-    public Order findById(String orderId) {
-        if (!orderData.containsKey(orderId)) {
-            throw new NoSuchElementException("No order found");
-        }
-        return orderData.get(orderId);
-    }
-
-    public List<Order> findAll() {
-        return new ArrayList<>(orderData.values());
-    }
+@Repository
+public interface OrderRepository extends JpaRepository<Order, UUID> {
 }
