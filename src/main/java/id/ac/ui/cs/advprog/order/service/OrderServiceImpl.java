1f34e0ec1a0974651aed70405d3d4ac4da27e5f4
deanitasekar <deanitasekar@gmail.com>
2025-05-02 18:59:25 +0700

    [RED] Add OrderServiceImpl skeleton

--- src/main/java/id/ac/ui/cs/advprog/order/service/OrderServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/order/service/OrderServiceImpl.java
@@ -2,31 +2,50 @@ package id.ac.ui.cs.advprog.order.service;
 
 import id.ac.ui.cs.advprog.order.model.Order;
 import id.ac.ui.cs.advprog.order.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+@Service
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
 
+    @Autowired
     public OrderServiceImpl(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
     }
 
     @Override
     public Order createOrder(Order order) {
-        orderRepository.save(order);
-        return order;
     }
 
     @Override
-    public Order updateStatus(String orderId, String newStatus) {
-        Order existing = orderRepository.findById(orderId);
-        existing.setStatus(newStatus);
-        orderRepository.save(existing);
-        return existing;
+    public List<Order> getCustomerOrders(UUID customerId) {
     }
 
     @Override
-    public Order findById(String orderId) {
-        return orderRepository.findById(orderId);
+    public Order findById(UUID orderId) {
+    }
+
+    @Override
+    public Order updateStatus(UUID orderId, String newStatus) {
+    }
+
+    @Override
+    public Order updateOrderDetails(UUID orderId, Order updatedOrder) throws Exception {
+    }
+
+    @Override
+    public Order changeTechnician(UUID orderId, UUID newTechnicianId) throws Exception {
+    }
+
+    @Override
+    public void cancelOrder(UUID orderId, UUID userId) throws Exception {
+    }
+
+    @Override
+    public Order completeOrder(UUID orderId, UUID technicianId, String repairReport) throws Exception {
     }
 }
