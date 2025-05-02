13abd3ca903c813ba6f52b359b05e5c28ea178fc
deanitasekar <deanitasekar@gmail.com>
2025-05-02 19:03:16 +0700

    [GREEN] Implement OrderService class

--- src/main/java/id/ac/ui/cs/advprog/order/service/OrderService.java
+++ src/main/java/id/ac/ui/cs/advprog/order/service/OrderService.java
@@ -1,9 +1,16 @@
 package id.ac.ui.cs.advprog.order.service;
 
 import id.ac.ui.cs.advprog.order.model.Order;
+import java.util.List;
+import java.util.UUID;
 
 public interface OrderService {
     Order createOrder(Order order);
-    Order updateStatus(String orderId, String newStatus);
-    Order findById(String orderId);
+    List<Order> getCustomerOrders(UUID customerId);
+    Order findById(UUID orderId);
+    Order updateStatus(UUID orderId, String newStatus);
+    Order updateOrderDetails(UUID orderId, Order updatedOrder) throws Exception;
+    Order changeTechnician(UUID orderId, UUID newTechnicianId) throws Exception;
+    void cancelOrder(UUID orderId, UUID userId) throws Exception;
+    Order completeOrder(UUID orderId, UUID technicianId, String repairReport) throws Exception;
 }
