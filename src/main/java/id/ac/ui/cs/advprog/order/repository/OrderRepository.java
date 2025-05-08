ecb1f3a2cf4bb32792d93fb6c49c7c5d120c8fe4
deanitasekar <deanitasekar@gmail.com>
2025-05-08 20:37:32 +0700

    [RED] Add OrderRepository skeleton

--- src/main/java/id/ac/ui/cs/advprog/order/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/order/repository/OrderRepository.java
@@ -1,19 +1,13 @@
 package id.ac.ui.cs.advprog.order.repository;
 
+import id.ac.ui.cs.advprog.order.enums.OrderStatus;
 import id.ac.ui.cs.advprog.order.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-    List<Order> findByCustomerId(UUID customerId);
-    List<Order> findByCustomerIdAndStatus(UUID customerId, String status);
-    List<Order> findByTechnicianId(UUID technicianId);
-    List<Order> findByTechnicianIdAndStatus(UUID technicianId, String status);
-    List<Order> findByStatus(String status);
-    Optional<Order> findById(UUID orderId);
 }
