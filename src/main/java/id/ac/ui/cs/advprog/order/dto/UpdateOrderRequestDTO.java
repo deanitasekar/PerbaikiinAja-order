dedd2d931af9242190e5abdf27d370663086ad69
deanitasekar <deanitasekar@gmail.com>
2025-05-08 20:46:40 +0700

    [RED] Add UpdateOrderRequestDTO skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/order/dto/UpdateOrderRequestDTO.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateOrderRequestDTO {
+}
