bb22acae3620993d0da201ee80a2908425c221b5
deanitasekar <deanitasekar@gmail.com>
2025-05-08 20:45:47 +0700

    [GREEN] Implement OrderListResponseDTO

--- src/main/java/id/ac/ui/cs/advprog/order/dto/OrderListResponseDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/order/dto/OrderListResponseDTO.java
@@ -5,9 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderListResponseDTO {
+    private List<OrderResponseDTO> orders;
+    private int count;
 }
