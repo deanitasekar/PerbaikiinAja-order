868f2dd58e6d510fcdb433e58d181529192bae64
deanitasekar <deanitasekar@gmail.com>
2025-05-08 20:32:17 +0700

    [GREEN] Implement OrderBuilder model

--- src/main/java/id/ac/ui/cs/advprog/order/model/OrderBuilder.java
+++ src/main/java/id/ac/ui/cs/advprog/order/model/OrderBuilder.java
@@ -13,20 +13,31 @@ public class OrderBuilder {
     private Date desiredServiceDate;
 
     public OrderBuilder setCustomerId(UUID customerId) {
+        this.customerId = customerId;
+        return this;
     }
 
     public OrderBuilder setItemName(String itemName) {
+        this.itemName = itemName;
+        return this;
     }
 
     public OrderBuilder setItemCondition(String itemCondition) {
+        this.itemCondition = itemCondition;
+        return this;
     }
 
     public OrderBuilder setIssueDescription(String issueDescription) {
+        this.issueDescription = issueDescription;
+        return this;
     }
 
     public OrderBuilder setDesiredServiceDate(Date desiredServiceDate) {
+        this.desiredServiceDate = desiredServiceDate;
+        return this;
     }
 
     public Order build() {
+        return new Order(this);
     }
 }
