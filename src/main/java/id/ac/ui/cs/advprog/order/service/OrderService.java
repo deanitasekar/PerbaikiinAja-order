f227ebfe549513056a4a5d296025f4d9bbc145be
deanitasekar <deanitasekar@gmail.com>
2025-05-08 21:04:17 +0700

    [RED] Add OrderService skeleton

--- src/main/java/id/ac/ui/cs/advprog/order/service/OrderService.java
+++ src/main/java/id/ac/ui/cs/advprog/order/service/OrderService.java
@@ -1,16 +1,20 @@
 package id.ac.ui.cs.advprog.order.service;
 
-import id.ac.ui.cs.advprog.order.model.Order;
-import java.util.List;
+import id.ac.ui.cs.advprog.order.dto.OrderRequestDTO;
+import id.ac.ui.cs.advprog.order.dto.UpdateOrderRequestDTO;
+import id.ac.ui.cs.advprog.order.dto.OrderResponseDTO;
+import id.ac.ui.cs.advprog.order.dto.OrderListResponseDTO;
+import id.ac.ui.cs.advprog.order.dto.ResponseDTO;
+
 import java.util.UUID;
 
 public interface OrderService {
-    Order createOrder(Order order);
-    List<Order> getCustomerOrders(UUID customerId);
-    Order findById(UUID orderId);
-    Order updateStatus(UUID orderId, String newStatus);
-    Order updateOrderDetails(UUID orderId, Order updatedOrder) throws Exception;
-    Order changeTechnician(UUID orderId, UUID newTechnicianId) throws Exception;
-    void cancelOrder(UUID orderId, UUID userId) throws Exception;
-    Order completeOrder(UUID orderId, UUID technicianId, String repairReport) throws Exception;
+    OrderResponseDTO createOrder(OrderRequestDTO orderRequest);
+
+    OrderResponseDTO getOrderById(UUID orderId);
+    OrderListResponseDTO getOrdersByCustomerId(UUID customerId);
+
+    OrderResponseDTO updateOrder(UUID orderId, UpdateOrderRequestDTO updateOrderRequest);
+
+    ResponseDTO cancelOrder(UUID orderId);
 }
