322fa5e74bcd9ba80c888eea065f99d5e12e2f35
deanitasekar <deanitasekar@gmail.com>
2025-05-08 20:44:42 +0700

    [GREEN] Implement OrderResponseDTO

--- src/main/java/id/ac/ui/cs/advprog/order/dto/OrderResponseDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/order/dto/OrderResponseDTO.java
@@ -1,13 +1,36 @@
 package id.ac.ui.cs.advprog.order.dto;
 
+import id.ac.ui.cs.advprog.order.enums.OrderStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderResponseDTO {
+    private UUID id;
+    private UUID customerId;
+    private UUID technicianId;
+    private String itemName;
+    private String itemCondition;
+    private String issueDescription;
+    private Date desiredServiceDate;
+    private OrderStatus status;
+    private String paymentMethod;
+    private String couponCode;
+    private String repairEstimate;
+    private Double repairPrice;
+    private String repairReport;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private String technicianName;
+    private Double technicianRating;
 }
