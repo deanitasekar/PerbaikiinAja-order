5cb27f97e7b15d8e8f2a8d184dadc16c47d11956
kaindraa <pcgsachio@gmail.com>
2025-05-09 16:56:23 +0700

    feat: implement CouponServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/order/service/CouponServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/order/service/CouponServiceImpl.java
@@ -4,9 +4,10 @@ import id.ac.ui.cs.advprog.order.enums.CouponType;
 import id.ac.ui.cs.advprog.order.factory.CouponStrategyFactory;
 import id.ac.ui.cs.advprog.order.model.Coupon;
 import id.ac.ui.cs.advprog.order.repository.CouponRepository;
-import id.ac.ui.cs.advprog.order.strategy.*;
+import id.ac.ui.cs.advprog.order.strategy.CouponStrategy;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 public class CouponServiceImpl implements CouponService {
@@ -21,40 +22,53 @@ public class CouponServiceImpl implements CouponService {
 
     @Override
     public Coupon create(Coupon coupon) {
-        return null;
+        if (coupon.getCode() == null || coupon.getCode().isBlank()) {
+            throw new IllegalArgumentException("Coupon code must not be empty");
+        }
+        return repo.save(coupon);
     }
 
     @Override
     public Coupon update(UUID id, Coupon coupon) {
-        return null;
+        if (repo.findById(id) == null) {
+            throw new NoSuchElementException("Coupon not found for update");
+        }
+        return repo.save(coupon);
     }
 
     @Override
     public void delete(UUID id) {
+        repo.deleteById(id);
     }
 
     @Override
     public List<Coupon> findAll() {
-        return null;
+        return repo.findAll();
     }
 
     @Override
     public List<Coupon> findAllValid() {
-        return null;
+        return repo.findAllValid();
     }
 
     @Override
     public List<Coupon> findByType(CouponType type) {
-        return null;
+        return repo.findByType(type);
     }
 
     @Override
     public Coupon findByCode(String code) {
-        return null;
+        return repo.findByCode(code);
     }
 
     @Override
     public double applyCoupon(Coupon coupon, double price) {
-        return 0;
+        if (!coupon.isValid()) return price;
+
+        CouponType type = coupon.getCoupon_type();
+        if (type == null) throw new IllegalArgumentException("Coupon type is null");
+
+        CouponStrategy strategy = strategyFactory.getStrategy(type);
+        return strategy.apply(price, coupon.getDiscount_amount());
     }
 }
