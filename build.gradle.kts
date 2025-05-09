a90b4382e1bdc5d8d102349cc5a920d5f0f63662
kaindraa <pcgsachio@gmail.com>
2025-05-09 18:12:02 +0700

    delete notification service related and fix gradlew build

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,25 +60,12 @@ dependencies {
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
     testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
 
-    tasks.register<Test>("unitTest") {
-        description = "Runs unit tests."
-        group = "verification"
-        filter {
-            excludeTestsMatching("*FunctionalTest")
-        }
-    }
 
-    tasks.register<Test>("functionalTest") {
-        description = "Runs functional tests."
-        group = "verification"
-        filter {
-            includeTestsMatching("*FunctionalTest")
-        }
-    }
 
-    tasks.withType<Test>().configureEach {
-        useJUnitPlatform()
-    }
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
 
 tasks.register<Test>("unitTest") {
