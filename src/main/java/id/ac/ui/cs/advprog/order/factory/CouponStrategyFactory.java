cfcf099d712060e047c96f058b939d0d5e66dfb1
kaindraa <pcgsachio@gmail.com>
2025-05-09 16:53:11 +0700

    feat: implement CouponStrategyFactory

--- src/main/java/id/ac/ui/cs/advprog/order/factory/CouponStrategyFactory.java
+++ src/main/java/id/ac/ui/cs/advprog/order/factory/CouponStrategyFactory.java
@@ -1,13 +1,24 @@
 package id.ac.ui.cs.advprog.order.factory;
 
 import id.ac.ui.cs.advprog.order.enums.CouponType;
-import id.ac.ui.cs.advprog.order.strategy.CouponStrategy;
+import id.ac.ui.cs.advprog.order.strategy.*;
 
 public class CouponStrategyFactory {
 
-
+    private final CouponStrategy fixedStrategy = new FixedCouponStrategy();
+    private final CouponStrategy percentageStrategy = new PercentageCouponStrategy();
+    private final CouponStrategy randomStrategy = new RandomCouponStrategy();
 
     public CouponStrategy getStrategy(CouponType type) {
-        return null;
+        if (type == null) {
+            throw new IllegalArgumentException("Coupon type must not be null");
+        }
+
+        return switch (type) {
+            case FIXED -> fixedStrategy;
+            case PERCENTAGE -> percentageStrategy;
+            case RANDOM -> randomStrategy;
+            default -> throw new IllegalArgumentException("Unsupported coupon type: " + type);
+        };
     }
 }
